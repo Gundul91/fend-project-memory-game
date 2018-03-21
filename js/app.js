64cb2711b4ab99c8b331fab85bb0a1b6d1085431
marco <gundul1@libero.it>
2018-03-21 17:43:17 +0100

    add random positioning of cards and some comments

--- js/app.js
+++ js/app.js
@@ -13,7 +13,6 @@
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -25,34 +24,56 @@ function shuffle(array) {
     return array;
 }
 
-addEventListenerList(document.querySelectorAll(".card"),"mousedown",function(event){
-  const openedCard = document.querySelector(".open");
-  if(this.classList.item(1)!="match" && this!==openedCard){
-    if(openedCard!=null && openedCard.firstElementChild.classList.item(1)==this.firstElementChild.classList.item(1))
-    {
-      hideCards(openedCard,this);
-      openedCard.classList.add("match");
-      this.classList.add("match");
-      this.removeEventListener("mousedown",)
-    }else if(openedCard!=null){
-      let thisEl=this;
-      this.classList.add("open","show");
-      setTimeout(function (){hideCards(openedCard , thisEl)}, 400);
-    }else{
-    this.classList.add("open","show");
+document.addEventListener('DOMContentLoaded', function () {
+  const deck = ["fa-diamond" ,"fa-diamond" , "fa-rocket" , "fa-rocket" , "fa-paperclip" , "fa-paperclip" , "fa-motorcycle" , "fa-motorcycle" , "fa-birthday-cake" , "fa-birthday-cake" , "fa-image" , "fa-image" , "fa-home" , "fa-home" , "fa-fire-extinguisher" , "fa-fire-extinguisher" , "fa-fire" , "fa-fire" , "fa-cloud" , "fa-cloud" , "fa-camera" , "fa-camera" , "fa-align-justify" , "fa-align-justify" , "fa-bell" , "fa-bell" , "fa-beer" , "fa-beer" , "fa-bed" , "fa-bed" , "fa-angellist" , "fa-angellist" , "fa-balance-scale" , "fa-balance-scale" , "fa-bomb" , "fa-bomb" , "fa-bicycle" , "fa-bicycle" , "fa-leaf" , "fa-leaf" , "fa-cube" , "fa-cube" , "fa-bolt" , "fa-bolt" , "fa-anchor" , "fa-anchor" , "fa-paper-plane-o" ,  "fa-paper-plane-o"];
+  // complete deck of cards 32 cards
+  let deckPortion = deck.slice(0 , 16);
+  deckPortion = shuffle(deckPortion);
+  const deckFrag = document.createDocumentFragment();
+  
+  for(let i=0; i<16;i++)
+  {
+    const card=document.createElement('li');
+    card.classList.add("card");
+    const tmpI = document.createElement('i');
+    tmpI.classList.add("fa",deckPortion[i])
+    card.append(tmpI);
+    deckFrag.appendChild(card);
+    console.log(card);
+  }// create a frag element with all the cards of this phase
+
+  document.querySelector(".deck").appendChild(deckFrag);
+
+  addEventListenerList(document.querySelectorAll(".card"),"mousedown",function(event){
+    const openedCard = document.querySelectorAll(".open");
+    if(this.classList.item(1)!="match" && this!==openedCard[0]){
+      if(openedCard.length==1 && openedCard[0].firstElementChild.classList.item(1)==this.firstElementChild.classList.item(1)) {// cards match
+        // openedCard.length==1 for how click faster than the animation of card coling
+        hideCards(openedCard[0],this);
+        openedCard[0].classList.add("match");
+        this.classList.add("match");
+      }else if(openedCard.length==1){// cards not match
+        let thisEl=this;
+        this.classList.add("open","show");
+        setTimeout(function (){
+          hideCards(openedCard[0] , thisEl)
+        }, 400);// leave a few seconds to see the cards
+        // if i only pass 'this' it point to the element Windows
+      }else{// first card
+        this.classList.add("open","show");
+      }
     }
-  }
+  });
+
+  function hideCards(oldCard,newCard){
+    oldCard.classList.remove("open","show");
+    newCard.classList.remove("open","show");
+  };
+
+  function addEventListenerList(list,ev,foo){
+    list.forEach((e)=>e.addEventListener(ev,foo));
+  }; // function for add eventlistener to a list of elements
 });
-
-function hideCards(oldCard,newCard){
-  oldCard.classList.remove("open","show");
-  newCard.classList.remove("open","show");
-};
-
-function addEventListenerList(list,ev,foo)
-{
-  list.forEach((e)=>e.addEventListener(ev,foo));
-}; //function for add eventlistener to a list of elements
 
 /*
  * set up the event listener for a card. If a card is clicked:
