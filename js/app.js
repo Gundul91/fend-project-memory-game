6b902682a08db234b82907529a358c96b39efc74
marco <gundul1@libero.it>
2018-03-26 22:31:05 +0200

    add some indentation and comments

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -28,10 +16,10 @@ document.addEventListener('DOMContentLoaded', function () {
   const fullDeck = ["fa-diamond" ,"fa-diamond" , "fa-rocket" , "fa-rocket" , "fa-paperclip" , "fa-paperclip" , "fa-motorcycle" , "fa-motorcycle" , "fa-birthday-cake" , "fa-birthday-cake" , "fa-image" , "fa-image" , "fa-home" , "fa-home" , "fa-fire-extinguisher" , "fa-fire-extinguisher" , "fa-fire" , "fa-fire" , "fa-cloud" , "fa-cloud" , "fa-camera" , "fa-camera" , "fa-align-justify" , "fa-align-justify" , "fa-bell" , "fa-bell" , "fa-beer" , "fa-beer" , "fa-bed" , "fa-bed" , "fa-angellist" , "fa-angellist" ];
   // complete deck of cards 32 cards
   let deck = document.querySelector(".deck");
-  let numCard=4;
+  let numCard = 4;
   let moves = document.querySelector(".moves");
-  let tmpRes=0;
-  let finalRes=0;
+  let tmpRes = 0;
+  let finalRes = 0;
   let star3class = document.querySelector(".result>.star3res");
   let star2class = document.querySelector(".result>.star2res");
   createDeck(numCard);
@@ -39,12 +27,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function createDeck(dimension) {
     deck.style.minHeight = dimension<10 ?  "27vw" : "50vw" ;
-    if(dimension!=4 && dimension!=32){
+    if(dimension != 4 && dimension != 32){
       deck.style.width = "50vw";
-    }else if(dimension==4){
+    }else if(dimension == 4){
       deck.style.width = "26vw";
     }else{
-      if(window.innerWidth>=window.innerHeight){ // change expantion for smartphone portrait
+      if(window.innerWidth >= window.innerHeight){ // change expantion for smartphone portrait
         deck.style.width = "94vw";
       }else{
         deck.style.minHeight = "94vw";
@@ -57,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     for(let i=0; i<dimension;i++)
     {
-      const card=document.createElement('li');
+      const card = document.createElement('li');
       card.classList.add("card");
       const tmpI = document.createElement('i');
       tmpI.classList.add("fa",deckPortion[i]);
@@ -71,16 +59,16 @@ document.addEventListener('DOMContentLoaded', function () {
   function addEventCards(){
   addEventListenerList(document.querySelectorAll(".card"),"mousedown",function(event){
     const openedCard = document.querySelectorAll(".open");
-    if(this.classList.item(1)!="match" && this!==openedCard[0]){
-      if(openedCard.length==1 && openedCard[0].firstElementChild.classList.item(1)==this.firstElementChild.classList.item(1)) {// cards match
+    if(this.classList.item(1)!= "match" && this !== openedCard[0]){
+      if(openedCard.length == 1 && openedCard[0].firstElementChild.classList.item(1) == this.firstElementChild.classList.item(1)) {// cards match
         // openedCard.length==1 for how click faster than the animation of card coling
         hideCards(openedCard[0],this);
         openedCard[0].classList.add("match");
         this.classList.add("match");
         moves.textContent++;
         checkStars();
-      }else if(openedCard.length==1){// cards not match
-        let thisEl=this;
+      }else if(openedCard.length == 1){// cards not match
+        let thisEl = this;
         this.classList.add("open","show");
         moves.textContent++;
         checkStars();
@@ -91,30 +79,30 @@ document.addEventListener('DOMContentLoaded', function () {
       }else{// first card
         this.classList.add("open","show");
       }
-      if(document.querySelectorAll(".match").length==numCard){
-        if(numCard!=32){
-          finalRes+=tmpRes;
-          numCard=numCard*2;
+      if(document.querySelectorAll(".match").length == numCard){
+        if(numCard != 32){
+          finalRes+= tmpRes;
+          numCard = numCard*2;
           cleanAndPlaceDeck();
           showStars();
         }else{
-          finalRes+=tmpRes;
-          finalRes=finalRes/4;
+          finalRes+= tmpRes;
+          finalRes = finalRes/4;
           document.querySelector(".result").style.display = "block";
-          if(finalRes<3 && finalRes>=2){
-            document.querySelector(".result>h2").innerHTML="Good!";
-            if(finalRes>=2.5){
+          if(finalRes < 3 && finalRes >= 2){
+            document.querySelector(".result>h2").innerHTML = "Good!";
+            if(finalRes >= 2.5){
               star3class.classList.replace("fa-star","fa-star-half");
             }else{
-              star3class.style.display="none";
+              star3class.style.display = "none";
             }
           }else if(finalRes<3){
-            star3class.style.display="none";
+            star3class.style.display = "none";
             document.querySelector(".result>h2").innerHTML="Bad, but you can do better!"
-            if(finalRes>=1.5){
+            if(finalRes >= 1.5){
               star2class.classList.replace("fa-star","fa-star-half");
             }else{
-              star2class.style.display="none";
+              star2class.style.display = "none";
             }
           }
         }
@@ -124,8 +112,8 @@ document.addEventListener('DOMContentLoaded', function () {
 }
 
 function cleanAndPlaceDeck(){
-  let cards=document.querySelectorAll(".card");
-  for(let i=0;i<cards.length;i++)
+  let cards = document.querySelectorAll(".card");
+  for(let i = 0; i < cards.length; i++)
   {
     cards[i].remove();
   }
@@ -133,7 +121,7 @@ function cleanAndPlaceDeck(){
     createDeck(numCard);
     addEventCards();
   }, 300);
-  moves.textContent=0;
+  moves.textContent = 0;
 }
 
   function hideCards(oldCard,newCard){
@@ -151,8 +139,8 @@ function cleanAndPlaceDeck(){
   });
 
   document.querySelector(".btnRestart").addEventListener("click",function (){
-    numCard=4;
-    finalRes=0;
+    numCard = 4;
+    finalRes = 0;
     cleanAndPlaceDeck();
     showStars();
     document.querySelector(".result").style.display = "none";
@@ -162,20 +150,20 @@ function cleanAndPlaceDeck(){
   });
 
   function checkStars(){
-    tmpRes=3;
-    if(parseInt(moves.textContent)>=Math.round(numCard*1.1)){
-      tmpRes=1;
-      document.querySelector(".star3").style.display="none";
-      document.querySelector(".star2").style.display="none";
-    }else if(parseInt(moves.textContent)>=Math.round(numCard*0.9)){
-      tmpRes=2;
-      document.querySelector(".star3").style.display="none";
+    tmpRes = 3;
+    if(parseInt(moves.textContent) >= Math.round(numCard*1.1)){
+      tmpRes = 1;
+      document.querySelector(".star3").style.display = "none";
+      document.querySelector(".star2").style.display = "none";
+    }else if(parseInt(moves.textContent) >= Math.round(numCard*0.9)){
+      tmpRes = 2;
+      document.querySelector(".star3").style.display = "none";
     }
   };
 
   function showStars(){
-    document.querySelector(".star3").style.display="inline";
-    document.querySelector(".star2").style.display="inline";
+    document.querySelector(".star3").style.display = "inline";
+    document.querySelector(".star2").style.display = "inline";
   };
 });
 
